981d5b354f01d2c66bd9f5e348443b071c6ce436
Jacob Sun <jacobsun000@gmail.com>
2023-12-18 16:34:00 +0800

    feat: Integrate database into server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { DB } from './db.js';
 
 const APP = express();
 const PORT = 4131;
@@ -31,36 +32,46 @@ APP.use((req, res, next) => {
 APP.use(express.static('resources'));
 
 const blogs = [
-  { 'id': 1, 'title': "Blog 1", 'text': "Content 1" },
-  { 'id': 2, 'title': "Blog 2", 'text': "Content 2" },
-  { 'id': 3, 'title': "Blog 3", 'text': "Content 3" },
-  { 'id': 4, 'title': "Blog 4", 'text': "Content 4" },
-  { 'id': 5, 'title': "Blog 5", 'text': "Content 5" },
-  { 'id': 6, 'title': "Blog 6", 'text': "Content 6" },
-  { 'id': 7, 'title': "Blog 7", 'text': "Content 7" },
-  { 'id': 8, 'title': "Blog 8", 'text': "Content 8" },
+  { 'id': 1, 'title': "Blog 1", 'body': "Content 1" },
+  { 'id': 2, 'title': "Blog 2", 'body': "Content 2" },
+  { 'id': 3, 'title': "Blog 3", 'body': "Content 3" },
+  { 'id': 4, 'title': "Blog 4", 'body': "Content 4" },
+  { 'id': 5, 'title': "Blog 5", 'body': "Content 5" },
+  { 'id': 6, 'title': "Blog 6", 'body': "Content 6" },
+  { 'id': 7, 'title': "Blog 7", 'body': "Content 7" },
+  { 'id': 8, 'title': "Blog 8", 'body': "Content 8" },
 ];
 
-// mainpage
-APP.get('/', function(req, res) {
+// home
+APP.get('/', async function(req, res) {
   const pageSize = 2;
-  const totalBlogs = 8;
-
   const curr = parseInt(req.query.page, 10) || 1;
+  // let blogs = await db.getPost({ sortBy: 'newest' });
+
+  const totalBlogs = blogs.length;
   const last = Math.ceil(totalBlogs / pageSize);
   const page = {
-    curr: curr,
-    last: last,
+    curr,
+    last,
     prev: curr > 1 ? curr - 1 : 1,
     next: curr < last ? curr + 1 : last
   };
 
-  res.render('main', { blogs: blogs, page: page });
+  let blogs_ = blogs.slice((curr - 1) * pageSize, curr * pageSize);
+
+  const previewBlogs = blogs_.map(blog => ({
+    title: blog.title,
+    body: blog.body.length > 100 ? blog.body.substring(0, 100) + '...' : blog.body,
+    image: blog.image,
+  }));
+
+  res.render('main', { blogs: previewBlogs, page });
 });
 
+// explore
 APP.get('/explore', function(req, res) {
   let searchText = req.query.search || '';
-  let sort = req.query.sort || 'date';
+  let sort = req.query.sort || 'newest';
 
   const pageSize = 2;
   let totalBlogs = 8;
